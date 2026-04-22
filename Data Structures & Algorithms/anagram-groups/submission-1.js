beaa3586db2945e73842c32628f5afff43e38453
pradeep90043 <95096691+pradeep90043@users.noreply.github.com>
2026-04-22 15:08:57 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const getKey = (str) => {
+            let key = new Array(26).fill(0);
+            for (let i = 0; i < str.length; i++) {
+                let index = str[i].charCodeAt(0) - 97;
+                key[index]++;
+            }
+            return key.join("#");
+        };
+        let hash = {};
+        for (let i = 0; i < strs.length; i++) {
+            let key = getKey(strs[i]);
+
+            if (!hash[key]) hash[key] = [];
+            hash[key].push(strs[i])
+        }
+        return Object.values(hash);
+    }
+}
