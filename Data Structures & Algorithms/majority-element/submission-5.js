1755271e0bef4693de1c59e1fdc91f1c22d8c5fb
pradeep90043 <95096691+pradeep90043@users.noreply.github.com>
2026-04-23 15:05:12 +0530

    Add: majority-element - submission-5

---
+++ Data Structures & Algorithms/majority-element/submission-5.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    majorityElement(nums) {
+        let count = 0;
+        let candidate;
+        for (let num of nums) {
+            if (count === 0) {
+                candidate = num;
+            }
+
+            count += candidate == num ? 1 : -1;
+        }
+        return candidate;
+    }
+}
