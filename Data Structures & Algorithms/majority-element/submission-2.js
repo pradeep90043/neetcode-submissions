bd74fe6b117d6a46b5027fac68e9cff38f7244c0
pradeep90043 <95096691+pradeep90043@users.noreply.github.com>
2026-04-23 14:54:13 +0530

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    majorityElement(nums) {
+        let rc = Math.floor(nums.length / 2);
+        nums.sort((a, b) => b - a);
+        let count = 0;
+        let number = nums[0];
+        for (let num of nums) {
+            console.log(count,num,number)
+            if (number == num) {
+                ++count;
+            } else if (count > rc) {
+                return num;
+            } else {
+                number = num;
+                count = 1;
+            }
+            if (count > rc) {
+                return num;
+            }
+        }
+    }
+}
